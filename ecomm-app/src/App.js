b53742593ff1dedbdb37cbc4dd2f54f18c4ff51a
Soumik Mallik <smkmallik@gmail.com>
2022-05-10 23:38:23 +0530

    Feat:- Added checkout changes
    Added checkout changes

--- ecomm-app/src/App.js
+++ ecomm-app/src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
-
+import Checkout from './components/Checkout/Checkout'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ ecomm-app/src/components/Checkout/Checkout.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Checkout.css";
+
+const Checkout = () => {
+  return (
+    <div className='checkout'>
+      <div className='checkout__left'>
+        <img
+          className='checkout__ad'
+          src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
+          alt=''
+        />
+
+        <div>
+          <h2 className='checkout__title'>Shopping Cart</h2>
+
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
+        </div>
+      </div>
+
+      <div className='checkout__right'>
+        <h2 className=''>Subtotal Component</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Checkout;
